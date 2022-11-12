e4ec907aec2e087b125f882ac0a16c8b1e2f7ccc
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-12 00:26:53 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Practice_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } // Main
+
+        // While Loop
+
+        // 1. + 2, - 5
+        // You will use a while loop for this exercise
+        // Create a variable that starts at 20
+        // Create a random object ( Random ), with a range of 0 to 10 ( Exclusive )
+        // In a while loop check to see if our starting variable is above 0 OR below 50
+        // Each time it loops a new random number between 0 and 9 is generated
+        // If the number is positive, add 2 to the starting variable
+        // If the number is negative, subtract 5 from the starting variable
+        // Each loop display the current loop, the random number, and the current value
+
+        // Example
+        // 20
+        // Count 1 : Random 5 : Current 15
+        // Count 2 : Random 2 : Current 17
+        // Count 3 : Random 4 : Current 19
+        // Count 4: Random 7 : Current 14
+        // Count 5: Random 9 : Current 9
+        // Count 6: Random 1 : Current 4
+        // etc...
+
+        // The loop should stop if our value is negative, or above 50
+
+        // For Loop
+
+        // 2. Fizz Buzz
+        // Ask the user for a positive int
+        // Using a for loop display from 1 to that number
+        // In the loop if i equals 3, display Fizz instead
+        // If i equals 5, display Buzz
+        // If i equals both 3 AND 5, display Fizz Buzz
+
+        // Example
+        // 1
+        // 2
+        // Fizz
+        // 4
+        // Buzz
+        // Fizz
+        // 7
+        // ...
+        // 13
+        // 14
+        // Fizz Buzz
+        // 16
+
+        // 3. Factorial
+        // In math a factorial is represented as a number and
+        // an exclamation point. Ex. 5!
+        // What this means is a number that is multiplied by itself
+        // minus 1, then again, all the way till 1
+        // Example: 5! = 5 x 4 x 3 x 2 x 1
+        // Result = 120
+
+        // Create a Factorial method that takes an int and returns an int
+        // Use a for loop that decreases and have it perform factorial math
+        // Example: int result = Factorial(3);
+        // Result: 6
+        // Logic: 3 x 2 x 1
+
+
+    } // class 
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Code_Practice_Loops
+ Programming 120 - While and For Loops
